f5502527ff26c3cf57d527f03d720c766a1e9858
theRealc0d3c <felixb@benno-gym.de>
2018-04-08 18:32:14 +0200

    Local style sheets
    Local style sheets

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    -->
+
+    <!-- local style sheets -->
+    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap-reboot.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap-reboot.min.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/edit_BS.css">
 
     <title>Testseite</title>
 
