a0ec713dc0ff59bb9b3416748c54e7c2857ee0a5
Lipp2 <32132961+Lipp2@users.noreply.github.com>
2018-04-09 21:56:20 +0200

    Bootstrap local, Navbarfix
    change color if you want

--- index.php
+++ index.php
@@ -3,12 +3,11 @@ require("header.php");
 ?>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
-  <nav id="#Navbar" class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+  <nav id="#Navbar" class="navbar navbar-expand-sm navbar-dark fixed-top">
       <a class="navbar-brand" href="#Home">Home</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
               <li class="nav-item">
